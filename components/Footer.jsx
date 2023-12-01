ef66f79781afdab6d0bc99d9c7da4c597cf5b185
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2023-12-01 08:35:52 -0800

    adding css classes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,31 +1,50 @@
 import React from 'react'
+import { FaInstagram } from 'react-icons/fa6'
 
 const Footer = () => {
     return (
-        <div className='absolute bottom-0 w-full h-1/6 bg-gray-500'>
-            <div className='flex flex-col justify-evenly'>
-                <div className='flex flex-row justify-evenly'>
-                    <div className='flex flex-col'>
-                        <p className='text-3xl font-bold'>Help</p>
-                        <p className='text-2xl'>Shipping</p>
-                        <p className='text-2xl'>Shipping</p>
-                        <p className='text-2xl'>Shipping</p>
-                        <p className='text-2xl'>Shipping</p>
+        <div className='footer'>
+            <div className='footer__container'>
+                <div className='footer__links_container'>
+                    <div className='footer__links'>
+                        <p className='footer__title'>Help</p>
+                        <p className='footer__help_link'>Shipping</p>
+                        <p className='footer__help_link'>Shipping</p>
+                        <p className='footer__help_link'>Shipping</p>
+                        <p className='footer__help_link'>Shipping</p>
                     </div>
-                    <div className='flex flex-col'>
-                        <p className='text-3xl font-bold'>Help</p>
-                        <p className='text-2xl'>Shipping</p>
-                        <p className='text-2xl'>Shipping</p>
-                        <p className='text-2xl'>Shipping</p>
-                        <p className='text-2xl'>Shipping</p>
+                    <div className='footer__links'>
+                        <p className='footer__title'>Help</p>
+                        <p className='footer__help_link'>Shipping</p>
+                        <p className='footer__help_link'>Shipping</p>
+                        <p className='footer__help_link'>Shipping</p>
+                        <p className='footer__help_link'>Shipping</p>
                     </div>
-                    <div className='flex flex-col'>
-                        <p className='text-3xl font-bold'>Help</p>
-                        <p className='text-2xl'>Shipping</p>
-                        <p className='text-2xl'>Shipping</p>
-                        <p className='text-2xl'>Shipping</p>
-                        <p className='text-2xl'>Shipping</p>
+                    <div className='mx-12 my-12'>
+                        <p className='about'>About Us</p>
+                        <p className='about__description'>
+                            Everything here at our store is hand made with love,
+                            <p>click this button to subscribe to our news letter, and</p>
+                            stay up to date when I make something new!
+                        </p>
                     </div>
+                </div>
+                <div className='footer__foot'>
+                    <div className='flex text-base'>
+                        <p>Shipping To: Local</p>
+                    </div>
+
+                    <div className='flex flex-row'>
+                        <p className='text-xl'>@ Rotting Pumpkin</p>
+                    </div>
+
+                    <div className='flex flex-row space-x-8'>
+                        <FaInstagram className='icon' />
+                        <FaInstagram className='icon' />
+                        <FaInstagram className='icon' />
+                        <FaInstagram className='icon' />
+                    </div>
+
                 </div>
             </div>
 
